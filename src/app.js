2781341fb8fa612533cbceb5be11be3a90c894af
gabo180 <gabrieldejesushernandez@gmail.com>
2022-04-30 01:00:00 +0000

    card-generator first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardBodyNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber === 11) return "J";
+    else if (randomNumber === 12) return "Q";
+    else if (randomNumber === 13) return "K";
+    else if (randomNumber === 1) return "A";
+    else return randomNumber;
+  };
+
+  const suitSelected = () => {
+    const randomNumber = Math.floor(Math.random() * 4) + 1;
+    if (randomNumber === 1) return "J";
+    else if (randomNumber === 12) return "Q";
+    else if (randomNumber === 13) return "K";
+    else if (randomNumber === 1) return "A";
+    else return randomNumber;
+  };
+  document.querySelector(".card-body").innerHTML = cardBodyNumber();
+  document.querySelector(".card-body").innerHTML = cardBodyNumber();
 };
